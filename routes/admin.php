20e0a21a1e0f64805af88b551e6877848d93b08d
Welson Oktario <welsonoktario@yahoo.com>
2022-06-06 18:21:15 +0700

    Update depedencies

--- routes/admin.php
+++ routes/admin.php
@@ -9,16 +9,15 @@ Route::redirect('/', '/home', 302);
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::resource('home', Admin\HomeController::class, ['as' => 'admin']);
     Route::resource('angkot', Admin\AngkotController::class, ['as' => 'admin']);
-    Route::resource('promo', Admin\PromoController::class, ['as' => 'admin']);
     Route::resource('transaksi', Admin\TransaksiController::class, ['as' => 'admin']);
     Route::resource('trayek', Admin\TrayekController::class, ['as' => 'admin']);
 
-    Route::group(['prefix' => 'akun'], function() {
+    Route::group(['prefix' => 'akun'], function () {
         Route::resource('driver', Admin\DriverController::class, ['as' => 'admin.akun']);
         Route::resource('user', Admin\UserController::class, ['as' => 'admin.akun']);
     });
 
-    Route::group(['prefix' => 'pengajuan'], function() {
+    Route::group(['prefix' => 'pengajuan'], function () {
         Route::resource('bonus', Admin\PengajuanBonusController::class, ['as' => 'admin.pengajuan']);
         Route::resource('driver', Admin\PengajuanDriverController::class, ['as' => 'admin.pengajuan']);
     });
